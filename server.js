3a9451f4cd3e1fb40cbfc4bb2a70b765ae7eb8e8
Your Name <your.email@example.com>
2026-09-17 13:21:55 +0100

    Remove migration route (migration complete)

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ const setupRoutes = require('./src/routes/setupRoutes');
 const trialRoutes = require('./src/routes/trialRoutes');
 const notificationRoutes = require('./src/routes/notificationRoutes');
 const materialRoutes = require('./src/routes/materialRoutes');
-const migrationRoute = require('./src/routes/migrationRoute');
 
 const app = express();
 app.set('trust proxy', 1);
@@ -73,7 +72,6 @@ app.use('/', setupRoutes);
 app.use('/', trialRoutes);
 app.use('/', notificationRoutes);
 app.use('/', materialRoutes);
-app.use('/api/migrate-data', migrationRoute);
 
 // Centralized Error Handler
 app.use((err, req, res, next) => {
